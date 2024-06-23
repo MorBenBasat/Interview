b15a1948d592c8f605d48c52e3ec44dad7a82d20
MorBenBasat <morbb1231@gmail.com>
2024-06-23 19:50:27 +0300

    Adding url's object

---
+++ src/helpers/Urls.ts
@@ -0,0 +1,7 @@
+export const Urls = {
+  login: "https://dash.cloudflare.com/login",
+  register: "https://dash.cloudflare.com/sign-up",
+  forgotEmail: "https://dash.cloudflare.com/forgot-email",
+  forgotPassword: "https://dash.cloudflare.com/forgot-password",
+  help: "https://support.cloudflare.com/hc/en-us",
+};
--- src/tests/todo.spec.ts
+++ src/tests/todo.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from "@playwright/test";
+import { Urls } from '../helpers/Urls';
 
 test("basic test", async ({ page }) => {
-  await page.goto(
-    "https://dash.cloudflare.com/login?_gl=1*13scmcm*_gcl_au*NzYwMTQwMDEwLjE3MTkxNjAxMDk.*_ga*MTA2OTYzOTg2OS4xNzE5MTYwMTA5*_ga_SQCRB0TXZW*MTcxOTE2MDExMC4xLjEuMTcxOTE2MDE1NC4wLjAuMA.."
+  await page.goto(Urls.forgotPassword  
   );
 
   await page.waitForTimeout(1000);

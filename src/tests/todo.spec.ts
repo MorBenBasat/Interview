6b979cc2b731fd52939bc780db3293b1f98e63d7
MorBenBasat <morbb1231@gmail.com>
2024-06-23 18:30:17 +0300

    First Commit

--- src/tests/todo.spec.ts
+++ src/tests/todo.spec.ts
@@ -3,5 +3,6 @@ import { test, expect } from "@playwright/test";
 test("basic test", async ({ page }) => {
   await page.goto("https://example.com");
   const title = await page.title();
+  const logoName = await page.locator("sss")
   expect(title).toBe("Example Domain");
 });
